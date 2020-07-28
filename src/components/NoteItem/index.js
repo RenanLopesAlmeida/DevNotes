1a2e1255f133c2301d625910b0a0f97a2f1a012e
Renan <RenanLopesAlmeida>
2020-07-27 21:45:36 -0300

    created a button to delete a note in ListScreen

--- src/components/NoteItem/index.js
+++ src/components/NoteItem/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {NoteBox, Title} from './styles';
+import React, {useCallback} from 'react';
+import {useDispatch} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import {NoteBox, Title, DeleteNoteButton} from './styles';
 
 export default ({data, index, onPress}) => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const handleDeleteButton = useCallback(() => {
+    dispatch({
+      type: 'DELETE_NOTE',
+      payload: {key: index},
+    });
+    navigation.goBack();
+  }, [dispatch, navigation, index]);
+
   return (
     <NoteBox onPress={() => onPress(index)}>
-      <Title>{data.title}</Title>
+      <>
+        <Title>{data.title}</Title>
+        <DeleteNoteButton onPress={handleDeleteButton}>
+          <FeatherIcon name="trash" size={22} color="#fff" />
+        </DeleteNoteButton>
+      </>
     </NoteBox>
   );
 };
--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -1,15 +1,29 @@
 import styled from 'styled-components/native';
 
 export const NoteBox = styled.TouchableHighlight`
-  padding: 16px;
+  height: 50px;
+  padding-left: 16px;
   background: #222;
-  border-radius: 10px;
+  border-radius: 15px;
   border-left-width: 10px;
   border-left-color: #7159c1;
   margin-bottom: 8px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Title = styled.Text`
   color: #fff;
   font-size: 17px;
+`;
+
+export const DeleteNoteButton = styled.TouchableHighlight`
+  height: 100%;
+  width: 50px;
+  background: #db4444;
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+  justify-content: center;
+  align-items: center;
 `;
